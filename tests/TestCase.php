fc80b1dcd8caa86ecc11fd117ac420afe6d4225d
Vasin <vasin.chatkittisuk@gmail.com>
2026-05-14 08:40:10 +0700

    ci: filter InvoiceTest only

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,14 +6,5 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        config([
-            'session.driver' => 'array',
-            'cache.default' => 'array',
-            'queue.default' => 'sync',
-        ]);
-    }
+    //
 }
